f0572f078fb493ee5deef4362017a9dff680ad6c
Hleb Ramniou <g.remniov@gmail.com>
2024-04-26 19:02:42 +0300

    Adjust servlet example

--- course/lesson4/src/main/java/org/andersenlab/TestServlet.java
+++ course/lesson4/src/main/java/org/andersenlab/TestServlet.java
@@ -1,6 +1,8 @@
 package org.andersenlab;
 
 import java.io.IOException;
+import java.util.stream.Collectors;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,6 +14,24 @@ public class TestServlet extends HttpServlet {
     response.setContentType("text/html");
     var out = response.getWriter();
     out.println("test");
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("application/json");
+    var requestBody = getBody(request);
+
+    var out = response.getWriter();
+    out.printf("""
+        {
+          "message": "Response from servlet",
+          "requestBody": %s
+        }
+        """, requestBody);
+  }
+
+  private String getBody(HttpServletRequest request) throws IOException {
+    return request.getReader().lines().collect(Collectors.joining());
   }
 
 }
